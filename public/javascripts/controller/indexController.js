aeafaff17e9adf3d5cf94edb03eb52cf2d20f2c8
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-09 00:42:08 +0300

    Create connection method in indexFactory

---
+++ public/javascripts/controller/indexController.js
@@ -0,0 +1,12 @@
+app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
+  const url = 'https://socketio-live-amongus.herokuapp.com'; // live
+  //const url = 'http://localhost:3000'; // development
+  indexFactory.connectSocket(url, {
+    reconnectionAttempts: 3,
+    reconnectionDelay: 600
+  }).then((socket) => {
+    console.log('connection successfully', socket);
+  }).catch((err) => {
+    console.log(err);
+  });
+}]);
--- public/javascripts/indexController.js
+++
@@ -1,3 +0,0 @@
-app.controller('indexController', ['$scope', ($scope) => {
-  const socket = io.connect('https://socketio-live-amongus.herokuapp.com/');
-}]);
