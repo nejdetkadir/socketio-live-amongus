285fb99f1d91de159743f80b80d48c759eb75b3c
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-09 01:11:27 +0300

    Add when connected event of user

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,12 +1,36 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
-  const url = 'https://socketio-live-amongus.herokuapp.com'; // live
-  //const url = 'http://localhost:3000'; // development
-  indexFactory.connectSocket(url, {
-    reconnectionAttempts: 3,
-    reconnectionDelay: 600
-  }).then((socket) => {
-    console.log('connection successfully', socket);
-  }).catch((err) => {
-    console.log(err);
-  });
+
+  $scope.messages = [];
+
+  $scope.init = () => {
+    const username = prompt('Please enter your username');
+    if (username)
+      initSocket(username);
+    else
+      return false;
+  };
+
+  function initSocket(username) {
+    const url = 'https://socketio-live-amongus.herokuapp.com'; // live
+    //const url = 'http://localhost:3000'; // development
+    indexFactory.connectSocket(url, {
+      reconnectionAttempts: 3,
+      reconnectionDelay: 600
+    }).then((socket) => {
+      //console.log('connection successfully', socket);
+      socket.emit('newUser', {
+        username
+      });
+      socket.on('newUser', (data) => {
+        const messageData = {
+          type: 0, //info, this mean message sent from server
+          username: data.username
+        };
+        $scope.messages.push(messageData);
+        $scope.$apply();
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
 }]);
--- src/socketAPI.js
+++ src/socketAPI.js
@@ -1,12 +1,24 @@
 const socketio = require('socket.io');
 const io = socketio();
-const socketAPI = {
-
-};
-
+const socketAPI = {};
 socketAPI.io = io;
+const users = [];
+
 io.on('connection', (socket) => {
   console.log('user connected.');
+  socket.on('newUser', (data) => {
+    const defaultData = {
+      id: socket.id,
+      position: {
+        x: 0,
+        y: 0
+      }
+    };
+    const userData = Object.assign(data, defaultData);
+    users.push(userData);
+    //console.log(users);
+    socket.broadcast.emit('newUser', userData);
+  });
 });
 
 module.exports = socketAPI;
